58a6bdeaa30dbbe0f0ffc2ad353bf3221eedd3d6
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2023-11-13 21:13:25 +0000

    Testing CircleCI config

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {} from './components/';
 import styles from './styles.module.scss';
 
 const App = () => {
+  // TESTING CIRCLECI CONFIG
   return (
     <main className={styles.app}>
       <h1>This is a Custom React App</h1>
